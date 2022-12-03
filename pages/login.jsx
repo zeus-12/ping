6cca7011c7043baac0031a4167af21bad23294f7
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-12-03 13:28:28 +0530

    login redirects for logged in users

--- pages/login.jsx
+++ pages/login.jsx
@@ -9,8 +9,9 @@ import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function SignIn() {
   const { user } = useAuthContext();
-  const { login, error } = useLogin();
   const router = useRouter();
+
+  const { login, error } = useLogin();
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -27,35 +28,39 @@ export default function SignIn() {
     }
   };
 
-  return (
-    <div className="min-h-screen gap-2 flex-col flex flex-1 justify-center items-center">
-      <div className="bg-gray-100 p-32 rounded-xl space-y-4">
-        <p className="text-4xl font-semibold tracking-tight">Login</p>
-        <Input
-          setValue={setUsername}
-          value={username}
-          label="Username"
-          type="text"
-        />
-        <Input
-          setValue={setPassword}
-          value={password}
-          label="Password"
-          type="password"
-        />
+  if (user) {
+    router.push("/");
+  } else {
+    return (
+      <div className="min-h-screen gap-2 flex-col flex flex-1 justify-center items-center">
+        <div className="bg-gray-100 p-32 rounded-xl space-y-4">
+          <p className="text-4xl font-semibold tracking-tight">Login</p>
+          <Input
+            setValue={setUsername}
+            value={username}
+            label="Username"
+            type="text"
+          />
+          <Input
+            setValue={setPassword}
+            value={password}
+            label="Password"
+            type="password"
+          />
 
-        <LoadingButton
-          variant="outlined"
-          fullWidth={true}
-          onClick={loginHandler}
-          loading={loading}
-          className="px-2 bg-blue-500 text-white hover:bg-blue-400"
-        >
-          Sign in
-        </LoadingButton>
+          <LoadingButton
+            variant="outlined"
+            fullWidth={true}
+            onClick={loginHandler}
+            loading={loading}
+            className="px-2 bg-blue-500 text-white hover:bg-blue-400"
+          >
+            Sign in
+          </LoadingButton>
 
-        {error && <p className="text-red-400">{error}</p>}
+          {error && <p className="text-red-400">{error}</p>}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
